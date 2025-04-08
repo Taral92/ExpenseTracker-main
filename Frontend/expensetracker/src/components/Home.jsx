cddf31525c79068ca429f95da9fa004a5aff24bb
Taral92 <taru.patel37225@gmail.com>
2025-04-08 16:46:28 +0530

    fix

--- Frontend/expensetracker/src/components/Home.jsx
+++ Frontend/expensetracker/src/components/Home.jsx
@@ -2,68 +2,70 @@ import React from "react";
 import Navbar from "./Navbar";
 import Logo from "./shared/Logo";
 import Createexpense from "./Createexpense";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 import { useDispatch } from "react-redux";
 import { setcategory, setmarkasdone } from "./app/expenseslice";
 import Expensetable from "./Expensetable";
 import Usegetexpenses from "@/hooks/Usegetexpenses";
 
-
 const Home = () => {
-    Usegetexpenses()
-  const dispatch=useDispatch()
-  const changecategoryhandler=(value)=>{
-     dispatch(setcategory(value))
-  }
-  const changeDonehandler=(value)=>{
-    dispatch(setmarkasdone(value))
-  }
+  Usegetexpenses();
+  const dispatch = useDispatch();
+  const changecategoryhandler = (value) => {
+    dispatch(setcategory(value));
+  };
+  const changeDonehandler = (value) => {
+    dispatch(setmarkasdone(value));
+  };
   return (
     <div>
       <div>
         <Navbar />
       </div>
-         <div className="flex py-2 items-center justify-between mr-2">
-         <div className="py-3 mx-5">
-        <Createexpense />
+      <div className="flex py-2 items-center justify-between mr-2">
+        <div className="py-3 mx-5">
+          <Createexpense />
+        </div>
+        <div className="flex gap-2 my-5 ml-2">
+          <h1 className="font-medium text-lg">Filter by : </h1>
+          <Select onValueChange={changecategoryhandler}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="select a category" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="rent">rent</SelectItem>
+              <SelectItem value="food">food</SelectItem>
+              <SelectItem value="study">study</SelectItem>
+              <SelectItem value="all">all</SelectItem>
+              <SelectItem value="others">others</SelectItem>
+              <SelectItem value="health">health</SelectItem>
+              <SelectItem value="transportation">transportation</SelectItem>
+              <SelectItem value="bill">bills</SelectItem>
+              <SelectItem value="shopping">shopping</SelectItem>
+              <SelectItem value="onlyfans">onlyfans</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select onValueChange={changeDonehandler}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Mark as Done" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="Done">Done</SelectItem>
+              <SelectItem value="Undone">Undone</SelectItem>
+              <SelectItem value="Both">Both</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-      <div className="flex gap-2 my-5 ml-2">
-      <h1 className="font-medium text-lg">Filter by : </h1>
-      <Select onValueChange={changecategoryhandler}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="select a category" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="rent">rent</SelectItem>
-          <SelectItem value="food">food</SelectItem>
-          <SelectItem value="study">study</SelectItem>
-          <SelectItem value="all">all</SelectItem>
-          <SelectItem value="others">others</SelectItem>
-          <SelectItem value="health">health</SelectItem>
-          <SelectItem value="transportation">transportation</SelectItem>
-          <SelectItem value="bill">bills</SelectItem>
-          <SelectItem value="shopping">shopping</SelectItem>
-          <SelectItem value="onlyfans">onlyfans</SelectItem>
-        </SelectContent>
-      </Select>
-      <Select onValueChange={changeDonehandler}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Mark as Done" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="Done">Done</SelectItem>
-          <SelectItem value="Undone">Undone</SelectItem>
-          <SelectItem value="Both">Both</SelectItem>
-         
-        </SelectContent>
-      </Select>
-
+      <div>
+        <Expensetable />
       </div>
-         
-         </div>
-         <div>
-         <Expensetable/>
-         </div>
     </div>
   );
 };
--- Frontend/expensetracker/src/hooks/Usegetexpenses.jsx
+++ Frontend/expensetracker/src/hooks/Usegetexpenses.jsx
@@ -3,30 +3,29 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+const Usegetexpenses = () => {
+  const { category, MarkasDone } = useSelector((store) => store.expenseslice);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchexpense = async () => {
+      try {
+        console.log(category, MarkasDone);
 
-const Usegetexpenses=()=>{
-   const{category,MarkasDone}=useSelector(store=>store.expenseslice)
-    const dispatch=useDispatch()
-    useEffect(()=>{
-           const fetchexpense=async()=>{
-            try {
-                console.log(category,MarkasDone);
-                
-               const res=await axios.get(`http://localhost:9000/api/user/getallx?category=${category}&done=${MarkasDone}`,
-                    {
-                        withCredentials:true
-                    }
-                )
-                if(res.data.success){
-                    dispatch(setexpenses(res.data.expenses))
-                }
-            } catch (error) {
-                console.log(error);
-                
-            }
-           }
-           fetchexpense()
-    },[dispatch,category,MarkasDone])
-    return null
-}
+        const res = await axios.get(
+          `http://localhost:9000/api/user/getallx?category=${category}&done=${MarkasDone}`,
+          {
+            withCredentials: true,
+          }
+        );
+        if (res.data.success) {
+          dispatch(setexpenses(res.data.expenses));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchexpense();
+  }, [dispatch, category, MarkasDone]);
+  return null;
+};
 export default Usegetexpenses;
